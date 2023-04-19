45a979b9330ea1f44cfc459d2b312a641048055b
airani1346 <69165916+airani1346@users.noreply.github.com>
2023-04-19 17:35:02 +0200

    Add files via upload

--- customdata.sh
+++ customdata.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-clish -c "set user admin shell ${admin_shell}" -s
-config_system -s "hostname=${gateway_name}&ftw_sic_key=${sic_key}&install_security_gw=true&gateway_daip=false&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&download_info=true"
-reboot
-#!/bin/bash
 ping 127.0.0.1 -c 120 > null
 
 sic_key=${sic_key}
